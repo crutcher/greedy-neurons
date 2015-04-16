e4cce5a4f2a4cbec5dde1539aacceb60bf0d45ef
Crutcher Dunnavant <crutcher@gmail.com>
2015-04-16 16:45:52 -0700

    Basic stupid QuickCheck work

--- SyntheticData.hs
+++ SyntheticData.hs
@@ -6,15 +6,7 @@ module SyntheticData
 
 import System.Random
 import Data.Matrix
-
--- Yields sequential chunks of size n from a list.
--- If the list is empty, produces no chunks.
--- If the list is non-empty, the last chunk may have [1, n] elements.
-chunksOf :: Int -> [a] -> [[a]]
-chunksOf _ [] = []
-chunksOf n xs = ys : chunksOf n yss
- where
-  (ys, yss) = splitAt n xs
+import Util (chunksOf)
 
 booleanClusterCenters :: (RandomGen g) => Int -> Int -> g -> [[Double]]
 booleanClusterCenters clusters dims g =
