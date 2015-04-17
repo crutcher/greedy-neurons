6f1dbe1e49b82c9ee9f07c50b61909c77da8dce6
Crutcher Dunnavant <crutcher@gmail.com>
2015-04-16 17:48:50 -0700

    unused module, test empty chunksOf

--- Util_Test.hs
+++ Util_Test.hs
@@ -5,20 +5,23 @@ import Util
 
 import Test.QuickCheck.Modifiers
   (Positive, getPositive, NonEmptyList, getNonEmpty)
-import Test.QuickCheck.Property (Testable)
 import Test.QuickCheck.All (quickCheckAll)
+
+prop_chunksOf_Empty :: Positive Int -> Bool
+prop_chunksOf_Empty pn = chunksOf n ([] :: [Char]) == ([] :: [[Char]])
+ where
+  n = getPositive pn
 
 prop_chunksOf_Flatten :: Positive Int -> [Char] -> Bool
 prop_chunksOf_Flatten pn xs = xs == concat (chunksOf n xs)
  where
   n = getPositive pn
 
-prop_chunksOf_AllButLastAreN :: Positive Int -> [Char] -> Bool
-prop_chunksOf_AllButLastAreN pn xs = all ((==n).length) (dropLast (chunksOf n xs))
+prop_chunksOf_AllButLastAreN :: Positive Int -> NonEmptyList Char -> Bool
+prop_chunksOf_AllButLastAreN pn xs = all ((==n).length) (init chunks)
  where
   n = getPositive pn
-  dropLast [] = []
-  dropLast xs = init xs
+  chunks = chunksOf n (getNonEmpty xs)
 
 prop_chunksOf_LastAtMostN :: Positive Int -> NonEmptyList Char -> Bool
 prop_chunksOf_LastAtMostN pn xs = (length $ last chunks) <= n
